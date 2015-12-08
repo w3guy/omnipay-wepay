1bd9007626756c4162dda80f64ad938f3c522460
Agbonghama Collins <me@w3guy.com>
2015-12-08 19:18:49 +0100

    added custom checkout support when a credit card token is supplied

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -5,52 +5,60 @@ namespace Omnipay\WePay\Message;
 /**
  * WePay Purchase Request.
  */
-class PurchaseRequest extends AbstractRequest
-{
-    public function getData()
-    {
-        $this->validate('transactionId', 'amount', 'currency', 'type', 'description', 'accountId');
+class PurchaseRequest extends AbstractRequest {
+	public function getData() {
+		$this->validate( 'transactionId', 'amount', 'currency', 'type', 'description', 'accountId' );
 
-        $data = array();
-        $data['account_id'] = $this->getAccountId();
+		$data               = array();
+		$data['account_id'] = $this->getAccountId();
 
-        // unique_id must be used with a preapproval or a tokenized credit card
-        // $data['unique_id']                       = $this->getTransactionId();
+		// unique_id must be used with a preapproval or a tokenized credit card
+		// $data['unique_id']                       = $this->getTransactionId();
 
-        $data['reference_id'] = $this->getTransactionId();
-        $data['amount'] = $this->getAmount();
-        $data['type'] = $this->getType();
-        $data['currency'] = $this->getCurrency();
-        $data['short_description'] = $this->getDescription();
-        $data['long_description'] = $this->getDescription();
-        $data['fee'] = array('fee_payer' => $this->getFeePayer());
-        $data['hosted_checkout'] = array();
-        $data['hosted_checkout']['redirect_uri'] = $this->getReturnUrl();
+		$data['reference_id']      = $this->getTransactionId();
+		$data['amount']            = $this->getAmount();
+		$data['type']              = $this->getType();
+		$data['currency']          = $this->getCurrency();
+		$data['short_description'] = $this->getDescription();
+		$data['long_description']  = $this->getDescription();
+		$data['fee']               = array( 'fee_payer' => $this->getFeePayer() );
 
-        if ($this->getCard()) {
-            $data['hosted_checkout']['prefill_info'] = array(
-                'name' => $this->getCard()->getName(),
-                'email' => $this->getCard()->getEmail(),
-            );
-        }
+		$token = $this->getToken();
+		if ( isset( $token ) && ! empty( $token ) ) {
+			$data['type']        = 'credit_card';
+			$data['credit_card'] = array(
+				'id' => $token
+			);
+		} else {
 
-        return $data;
-    }
+			$data['hosted_checkout']                 = array();
+			$data['hosted_checkout']['redirect_uri'] = $this->getReturnUrl();
 
-    public function sendData($data)
-    {
-        try {
-            $response = $this->httpClient->post(
-                $this->getEndpoint(),
-                $this->getApiHeader(),
-                json_encode($data)
-            )->send();
+			if ( $this->getCard() ) {
+				$data['hosted_checkout']['prefill_info'] = array(
+					'name'  => $this->getCard()->getName(),
+					'email' => $this->getCard()->getEmail(),
+				);
+			}
+		}
 
-            return new PurchaseResponse($this, $response->json());
-        } catch (\Guzzle\Http\Exception\BadResponseException $e) {
-            $response = $e->getResponse();
+		return $data;
+	}
 
-            return new PurchaseResponse($this, $response->json());
-        }
-    }
+	public function sendData( $data ) {
+		try {
+			$response = $this->httpClient->post(
+				$this->getEndpoint(),
+				$this->getApiHeader(),
+				json_encode( $data )
+			)->send();
+
+			return new PurchaseResponse( $this, $response->json() );
+		}
+		catch ( \Guzzle\Http\Exception\BadResponseException $e ) {
+			$response = $e->getResponse();
+
+			return new PurchaseResponse( $this, $response->json() );
+		}
+	}
 }
