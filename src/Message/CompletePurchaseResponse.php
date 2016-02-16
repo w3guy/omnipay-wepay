f30538c23a739b9f8c6fefdfc01b89a8e5089df0
Agbonghama Collins <me@w3guy.com>
2016-02-16 12:51:52 +0100

    added error messages for failed, cancelled and expired payment state

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -11,8 +11,13 @@ class CompletePurchaseResponse extends AbstractResponse
 {
     public function isSuccessful()
     {
-        return isset($this->data['state'])
-               && in_array($this->data['state'], array('authorized', 'reserved', 'captured'));
+        return is_string($this->getState())
+               && in_array($this->getState(), array('authorized', 'reserved', 'captured'));
+    }
+
+    public function getState()
+    {
+        return isset($this->data['state']) ? $this->data['state'] : null;
     }
 
     public function getTransactionReference()
@@ -32,6 +37,25 @@ class CompletePurchaseResponse extends AbstractResponse
 
     public function getMessage()
     {
-        return isset($this->data['error_description']) ? $this->data['error_description'] : null;
+        // when the state of a payment falls within these, no error message ($this->data['error_description'] is blank)
+        // is returned hence these default error messages.
+        /*
+         * @todo include test for these. Just too lazy to write these test.
+         */
+        switch ($this->getState()) {
+            case 'failed':
+                $error = 'Payment failed. Try again.';
+                break;
+            case 'expired':
+                $error = 'Checkout expired. Try again.';
+                break;
+            case 'cancelled':
+                $error = 'Payment cancelled.';
+                break;
+            default:
+                $error = isset($this->data['error_description']) ? $this->data['error_description'] : null;
+        }
+
+        return $error;
     }
 }
